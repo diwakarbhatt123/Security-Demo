34a8f033a59f19032023e69e43c7c127edd60641
Diwakar Bhatt <diwakar.bhatt@cloudwords.com>
2017-07-15 17:39:30 +0530

    Custom login form

--- src/main/java/com/springsecuritydemo/controller/HomeController.java
+++ src/main/java/com/springsecuritydemo/controller/HomeController.java
@@ -33,6 +33,13 @@ public class HomeController {
         return modelAndView;
     }
 
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
     @RequestMapping(value = "/accessdenied", method = RequestMethod.GET)
     public ModelAndView accessDenied() {
         ModelAndView modelAndView = new ModelAndView();
