728d5b5b9be6cc96c8d26c83a9d5c67e9fc2db1f
Diwakar Bhatt <diwakar.bhatt@cloudwords.com>
2017-07-16 15:47:03 +0530

    Configured remember me in Java

--- src/main/java/com/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/com/springsecuritydemo/config/SecurityConfig.java
@@ -2,10 +2,15 @@ package com.springsecuritydemo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 /**
  * Created by diwakar on 16/07/17.
@@ -15,6 +20,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     @Qualifier("customUserDetailService")
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private DataSource dataSource;
 
     @Autowired
     public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
@@ -29,8 +37,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/admin/**").access("hasRole('ADMIN')")
                 .antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')")
                 .and().formLogin().loginPage("/login").usernameParameter("userID").passwordParameter("password")
+                .and().rememberMe().rememberMeParameter("remember-me").rememberMeCookieName("remember-me").tokenRepository(persistentTokenRepository()).tokenValiditySeconds(86400)
                 .and().csrf()
                 .and().exceptionHandling().accessDeniedPage("/WEB-INF/views/jsp/accessdenied.jsp");
 
+    }
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl tokenRepositoryImpl = new JdbcTokenRepositoryImpl();
+        tokenRepositoryImpl.setDataSource(dataSource);
+        return tokenRepositoryImpl;
     }
 }
---
+++ src/main/java/com/springsecuritydemo/domain/PersistantLogins.java
@@ -0,0 +1,71 @@
+package com.springsecuritydemo.domain;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by diwakar on 16/07/17.
+ */
+@Entity
+@Table(name = "persistent_logins")
+public class PersistantLogins {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String username;
+    private String series;
+    private String token;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date last_used;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Date getLast_used() {
+        return last_used;
+    }
+
+    public void setLast_used(Date last_used) {
+        this.last_used = last_used;
+    }
+
+    @Override
+    public String toString() {
+        return "PersistantLogins{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", series='" + series + '\'' +
+                ", token='" + token + '\'' +
+                ", last_used=" + last_used +
+                '}';
+    }
+}
